c231cadd4af84d4ae896b882c6315d29b6fe7fa6
Harats15061 <muryzhnikova-natalya@yandex.ru>
2023-01-24 01:51:26 +0500

    пушим

--- практическая работа 1/Задание 2.py
+++
@@ -1,8 +0,0 @@
-#Пользователь вводит время в секундах. Переведите время в часы, минуты, секунды и выведите в формате чч:мм:сс.
- #Используйте форматирование строк.
-time_translation = int(input("Введите время в секундах: "))
-hour = time_translation // 3600
-minute = (time_translation % 3600) // 60
-second = (time_translation % 3600) % 60
-
-print(f"{hour:02}:{minute:02}:{second:02}")
